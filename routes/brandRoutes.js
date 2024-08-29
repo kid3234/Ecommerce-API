0759920a51dbd9ade00a62848a553d4261dff663
habjk <habibjmk@gmail.com>
2024-08-29 10:43:50 +0300

    update

--- routes/brandRoutes.js
+++ routes/brandRoutes.js
@@ -6,8 +6,8 @@ import isAdmin from "../middlewares/isAdmin.js";
 const brandRoutes = express.Router()
 
 brandRoutes.post('/',isLoggedIn,isAdmin,createBrand);
-brandRoutes.get('/',isLoggedIn,getAllBrands);
-brandRoutes.get('/:id',isLoggedIn,getBrand);
+brandRoutes.get('/',getAllBrands);
+brandRoutes.get('/:id',getBrand);
 brandRoutes.put('/:id',isLoggedIn,isAdmin,updateBrand);
 brandRoutes.delete('/:id',isLoggedIn,isAdmin,deleteBrand);
 
